cdf528ac73ffcc0bd98025c5f2df97e052b32d2d
David <agentdaffaires@gmail.com>
2017-03-29 10:18:07 -0700

    Create argv_stuff.s

---
+++ argv_stuff.s
@@ -0,0 +1,46 @@
+; David @InfinitelyManic
+section .bss
+section .data
+        fmt:    db      "%s", 10, 0
+
+section .text
+        global main
+        extern printf
+
+main:
+        nop
+        push rbp
+        mov rbp, rsp
+        sub rsp, (16 * 1)
+        ; *********************
+        mov rcx, rsi            ; get argv pointer
+        lea rdi, [rcx]          ; get argv 1
+.loop:
+        mov [rsp], rcx          ; save argv
+
+        call _write
+
+        mov rcx, [rsp]          ; save argv
+
+        add rcx, 8              ; inc to next pointer
+        mov rsi, [rcx]          ; get next string
+        cmp rsi, 0              ; is null
+        jnz .loop               ; else repeat
+        ; *********************
+        xor eax, eax
+        leave
+        ret
+
+_exit:
+        mov rax, 60
+        xor rdi, rdi
+        syscall
+_write:; rdi rsi rdx rcx r8 r9
+        push rbp
+        mov rbp, rsp
+        lea rdi, [fmt]
+        xor rax, rax
+        call printf
+        xor rax, rax
+        leave
+        ret
